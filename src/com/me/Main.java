0af8ebb471462e930f6c326fd61f437592cfb970
Holly Chaffee <saltdc95@gmail.com>
2020-12-11 16:29:12 -0500

    Completed challenge

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -3,10 +3,10 @@ package com.me;
 public class Main {
 
     public static void main(String[] args) {
-		byte byteValue = 25;
-		short shortValue = 235;
-		int intValue = 1025;
-		long longTotal = 50_000L + 10L * (byteValue + shortValue + intValue);
+        byte byteValue = 25;
+        short shortValue = 235;
+        int intValue = 1025;
+        long longTotal = 50_000L + 10L * (byteValue + shortValue + intValue);
         System.out.println(longTotal);
 
         short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
